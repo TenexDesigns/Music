383c1e63def6641694b74197a40393b70eb6ef36
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-28 21:05:58 +0300

    Create progression patterns.java

---
+++ progression patterns.java
@@ -0,0 +1,25 @@
+Progression 1 (uplifting) / 5, 6, 7, 1 
+Progression 2 (serious) / 6, 7, 1, 7 
+Progression 3 (tense) / root chord 
+Progression 4 (sad) / 1, 7, 4, 6 
+Progression 5 (feel good) / 6, 3, 7, 1 
+Progression 6 (sad euphoric) / 6, 1, 7, 5 
+Progression 7 (pump) / 1, 6, 4 
+Progression 8 (happy pump) / 1, 3, 6
+Progression 9 (chill) / 6,1,7,1 
+Progression 10 (happy) 4,5,1
+
+
+
+
+
+1. 0:18 euphoric/uplifting
+2. 1:25 serious/deep
+3. 2:52 tense/suspenseful
+4. 3:47 sad/sorrowful
+5. 4:45: feel good/joyful
+6. 6:13 feel good/sorrowful
+7. 7:15 pump up/dark
+8. 8:44 pump up/joyful
+9. 9:57 relaxed/chill
+10. 11:25 happy/cheerful
