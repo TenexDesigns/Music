8acb24f7ae65e14d0839eeb6312bccc487254ea7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-29 08:05:05 +0300

    Create Music Theory.java

---
+++ Music Theory.java
@@ -0,0 +1,46 @@
+steps
+  Half step -- A to Bb
+ Whole Step - from A to B
+
+
+    Notes 
+    # - Sharp
+    b - Flat
+                                                              440Hz                                                           880Hz
+                                                               |                                                               |
+    ORDER OF A NOTES KEYBOARD                                  |                                                               |
+                                                               |                                                               |
+      F#       Ab     Bb                Db       Eb            |   F#        Ab      Bb                 Db      Eb             |  F#      Ab      Bb                 Db       Eb                F#       Ab     Bb                 Db      Eb                                                           
+    F      G      A        B        C        D        E        F        G      A        B        C          D         E        F        G      A        B        C        D        E        F        G       A        B        C        D        E  
+
+
+
+    OCTAVE 
+    thE SAME NOTE THAT IS 12 NOTES APART
+
+    They vare the smae note - They just ound diffrent because they double as they go up and half as they go doan
+
+
+
+
+    Scales
+
+    What Notes sound bettr together chronologically
+C - Scale  
+  c major scale
+  
+  C D E F G A B
+  
+  The Nashvile Number System
+    I     II    III   IV     V      VI   VII
+    C     D     E     F      G      A    B
+  
+
+
+  C MIBOR SCALE
+  
+  C  D Eb F F# G Ab Bb
+
+
+
+    
