88dfd1fd0790eab350c571eb1405d2fe55df135e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-05-29 21:08:44 +0300

    Update Music Theory.java

--- Music Theory.java
+++ Music Theory.java
@@ -37,10 +37,120 @@ C - Scale
   
 
 
-  C MIBOR SCALE
+  C MINOR SCALE
   
   C  D Eb F F# G Ab Bb
 
 
+  G MAJOR
 
-    
+  W    W    H  W   W   E    W        H
+  G    A    B  C   D   E     F#      G
+
+  HALF STEP - NEXT KEY
+  WHOLE STEP - sKIP A KEY
+  
+
+
+
+
+  MOST COMMEON CHORDS
+
+  I IV V VI
+
+
+
+  DRUMS
+  BASS
+  MIDS
+  EFFECT
+  VOX
+
+
+OUTLINE
+  TEMPO
+  SCALE
+  NOTES
+  FREQUENCIES
+  CHORD
+
+
+
+  MAJARO
+  3-2
+
+
+  MINOR 
+  2-3
+
+  CHORD PROGREESSION, AND THE MOST USED CHRODS
+
+
+  MOST SONGS HAVE 4 CHORDS THAT REPEAT OVER AND OVER
+  FACE
+  F - MAJOR A-MINOR C-MAJOR E-MINOR
+  G-MAJOR
+
+
+  3-NOTES - CFG
+  5-NOTES
+  7-NOTES
+
+
+  MELODY AND CHORDS = SONG
+
+
+  SCALES ARE MDE OF NOTES
+  THERE ARE 12 NOTE ---CDEFGAB,F#,Ab,bB,Db,Eb,
+
+
+
+  kEY - oRAGANISATION OF SEVEN NOTES THAT SOUND GGGO TOGETHER
+
+    MELODY, RYTHM , CHRODS = SONGG
+
+
+piano chords
+
+  there are many, but you will not use all of them 80% of the time
+  20% of this chords are used 80% of the time
+
+  6- gold chrods --MASTER THEIS 6 AND YOU WILL BE ABLE TO PLAY 84% OF ALL SONGS
+
+  FACEDG  - may peanist are able to play almost any song with this  ,  legendar for 4 chord progression
+  F-MAJOR
+  A-MINOR
+  C-MAJOR 
+  E-MINOR
+  D-MINOR
+  G-MAJOR
+
+
+SILVER CHORS --- NOTS USED AS OFTEN
+
+
+  MAJORS --D E - A
+  MICORS - C F G
+
+
+
+  BRONZE CHORDS ,ARE USED EVEN LESS
+
+  B-MAJOR
+  Bb/A# 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
